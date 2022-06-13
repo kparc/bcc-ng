b5338efc7133480e2105a47df8210cbb38a4ace5
kelas <me@kel.as>
2022-06-13 20:45:00 +0200

    [+] d

---
+++ d/ev.c
@@ -0,0 +1,36 @@
+/*
+#define G(n,T,t) C(n,((t*)L[15&r])[l(*s++)]=T[r>>4])C(n+1,T[r>>4]=((t*)L[15&r])[l(*s++)])
+K1(ev){
+ S s=xy;ZF M[16];ZJ A[999],*a=A+999,L[16],c,f;
+ I b(){f&=7;R!f||(4>f?c==f-2:c!=f-6);}
+ J l(I c){R 16>c?L[c]:32>c?(I)xJ[2+c-16]:124>c?(J)G[c-'a']:c-128;}
+ J j2(I i,J x,J y){R!i?y:1==i?x+y:2==i?x-y:3==i?x*y:4==i?x>>y:5==i?x<<y:x&y;}
+ F m(I c){R 16>c?M[c]:32>c?   xF[2+c-16]:*(F*)(G+c-'a');}
+ F f2(I i,F x,F y){R!i?y:1==i?x+y:2==i?x-y:3==i?x*y:x/y;}
+ W(*s){
+  f=*s++;I r=*s++;
+  $(1==f,$(25<r,*L=(J)l1((K)L[1],(K)L[2],(K)L[3]))ev(G[r]))
+  $(16>f,L[r]=b())
+  $(24>f,s+=b()?(signed char)r:0)
+  $(32>f,s+=3;s+=b()?*((I*)s-1):0)
+  $(64>f,
+   S(31&f,C(0,*--a=L[r])C(1,L[r]=*a++)C(2,c=L[r]?1:0)C(3,oi(L[r]))                  C(6,J j=l(*s++);c=L[r]<j?-1:L[r]>j)G(12,L,I)G(14,L,J),L[r>>4]=j2(f-48,L[15&r],l(*s++))))
+   S(31&f,C(0,*--a=M[r])C(1,M[r]=*a++)C(2,c=M[r]?1:0)C(3,of(M[r])) C(4,M[r]=L[*s++])C(6,F f=m(*s++);c=M[r]<f?-1:M[r]>f)                  ,M[r>>4]=f2(f-80,M[15&r],m(*s++)))}
+
+ R x=xy,KS<xu?(K)*L:KF==xu?kf(*M):ki((I)*L);}
+V1(lnk){}
+K ps(S s){K z;I a=0;
+ I ln(S s){I c=*s&31;R!*s?1:32>*s||4>c?2:15>*s?5:3;}
+ I j(I t){R KF==t?32:0;}
+ C B=2,RET=0,JJ[]={16,17,18,19,24,21,22,23};
+ K cc(I o,I r){R c2(8+o,r);}
+ K Jj(K x,I n){R c5(8+x[xn],n-3);}
+ K cll(I c){R c2(1,c);}
+ K psh(I t,I r){R c2(32+j(t),r);}
+ K pop(I t,I r){R c2(33+j(t),r);}
+ K tst(I t,I r){R c2(34+j(t),r);}
+ K sh(I t,I r){R c2(35+j(t),r);}
+ K cv(I x,I y){R c3(36+32,x,y);}
+ K cm(I t,I x,I y){R c3(38+j(t),x,y);}
+ K o2(I t,I o,I r,I x,I y){R c3(48+o+j(t),16*r+x,y);}
+*/
