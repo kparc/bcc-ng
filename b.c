18e85f7299ed05d8eb5132862c1ce6701ab3fbd6
kelas <me@kel.as>
2022-06-16 02:13:22 +0200

    [+] doc

--- b.c
+++ b.c
@@ -40,10 +40,10 @@ ZK h(I o,I x,I y){R j2(
  // 2nd arg (y) in 32 bit address following the instruction
  c5(m(0,x,5),y));}
 
-ZK i(I o,I x,I y){R rex(16>x?x:0,0,16>y?y:0,h(o,16>x?A[x]:x-16,16>y?A[y]:y));}     //!< opcode o, arguments xy (maybe rex)
+ZK i(I o,I x,I y){R rex(16>x?x:0,0,16>y?y:0,h(o,16>x?A[x]:x-16,16>y?A[y]:y));}     //!< o opcode, xy arguments (maybe rex)
 ZK psh(I t,I x){R rex(0,0,x,c1(0x50+(7&A[x])));}                                   //!< push from reg A[x]
 ZK pop(I t,I x){R rex(0,0,x,c1(0x58+(7&A[x])));}                                   //!< pop to reg A[x]
-ZK jmp(I n){R n<-128||n>127?c5(JJ[4],0>n?n-3:n):c2(*JJ,n);}                        //!< c5|c2 long|shortjmp
+ZK jmp(I n){R n<-128||n>127?c5(JJ[4],0>n?n-3:n):c2(*JJ,n);}                        //!< jump (c5 long c2 short)
 ZK cc(I o,I x){R j2(i(0x0f20+JJ[o],16,x),i(0x0fb6,x,x));}                          //!< cond (set byte on conditon functions: 0x0f90,...)
 ZK tst(I t,I x){R KF==t?AB("tst"):i(0x85,x,x);}                                    //!< test (sets sz if x is not zero), nyi for floats
 ZK Jj(K x,I n){R cj(0x0f,c5(16+xC[xn],n-4));}
@@ -67,8 +67,11 @@ ZK o2(I t,I o,I r,I x,I y){K z;//O("o2: t=%c o=%d r=%d x=%p y=%p\n"," chijefs CH
   //       mov  add  sub  mul  div  cmp      i2f
   h((C[]){0x10,0x58,0x5c,0x59,0x5e,0x2e,0,0,0x2a}[o],r,y)))
  I a=126<y,s;
- P(0<=o&&r==x&&(!a||3-o),4-o?o2f(o,r,y):129-y?AB("/"):i(0xd1,16+7,r))    //!< shl|shr
- P(0<o&&r==y,z=o2f(o,r,x),2-o?z:j2(z,i(0xf7,16+3,r)))                    //!< neg
+ P(0<=o&&r==x&&(!a||3-o),                                    //!< shl|shr
+    4-o?o2f(o,r,y):                                          //!< 4 is % forces arguments to be floats, for integers means right shift (/)
+    129-y?AB("/"):                                           //!< shift by one or fail
+    i(0xd1,16+7,r))                                          //!< right shift by one
+ P(0<o&&r==y,z=o2f(o,r,x),2-o?z:j2(z,i(0xf7,16+3,r)))        //!< neg
  P((a?3:1)<o,j2(o2f(0,r,x),o2f(o,r,y))) //     mov  mov      lea imul
  R s=0<o?0:3+(o+1)/2,rex(r,a?0:y,x,c3(0>o?1&o?0x8b:0x89:3-o?0x8d:0x6b,m(3-o?a:3,A[r],a?A[x]:4),a?(2-o?y-128:128-y)<<s:m(s,A[y],A[x])));}
 
@@ -77,8 +80,12 @@ ZK cm(I t,I x,I y){R o2(t,5,x,x,y);}ZK cv(I x,I y){R o2(KF,8,x,x,A[y]);}ZK sh(I 
 
 //!comparison|operator|funcall dispatch
 ZK O2(I t,I f,I r,K x,K y){I i=Ay?yi:yu; //!< y is either value or function name
- R u(r,j3(Ay?c0():y,x,
-  10>f?o2(t,f,r,xu,i):j2(cm(t,xu,i),16>r?cc(f-9,r):c1(f-9))));} //!< comparison or function call (^ is ops|conds boundary in U())
+ R u(r,j3(
+    Ay?c0():y,
+    x,
+    10>f?o2(t,f,r,xu,i):                        //!< function
+    j2(cm(t,xu,i),16>r?cc(f-9,r):               //!< comparison
+    c1(f-9))));}
 
 ZK ZR(I t,C r){R u(r,o2(t,2,r,r,r));}           //!< set register r of type t to zero (2 is subtract, i.e. r-r=0)
 ZK MV(I t,I r,K y){R O2(t,0,r,u(r,c0()),y);}    //!< move value y with type t to register r (0 is mov, the first arg is empty array)
@@ -87,7 +94,8 @@ ZK SH(I t,K y){R u(yu,j2(y,sh(t,yu)));}         //!< nyi
 ZI1(hh){I t=T[xi-'a'];R 14==t?-2:13==t?-4:2*t-26;}
 ZI1(q){I i=xi-'a';R Ax?26u>i&&L[i]?L[i]:0:':'==*xC?I(xy):0;}
 
-I1(t){I a=xi-'a';                         //!< determine type of x
+//!type of x
+I1(t){I a=xi-'a';
  I r=!Ax?xu:                              //!< not an atom is a function: xu holds rettype
   126<xi?KI:                              //!< small integer atom encoded in a single byte
   26u>a&&T[a]?T[a]:                       //!< local variable
@@ -95,7 +103,8 @@ I1(t){I a=xi-'a';                         //!< determine type of x
     zK[2+xi-16])?Ax:                      //!< function argument (additional element of z)
   xt+8;R r;}                              //!< array (element type + 4th bit)
 
-ZK e(I r,K x){K y=d(r,x);if(Ay){          //!< expr x to register r
+//!expr x to register r
+ZK e(I r,K x){K y=d(r,x);if(Ay){
     I qz=128==yi||(32>yi&&!zK[2+yi-16]);  //!< if y is zero, either coded as a small int(128), or in the z array
     R qz?ZR(t(x),r)                       //!< set register to zero
         :MV(t(x),r,y);                    //!< otherwise move value to register
@@ -106,8 +115,7 @@ ZK E(I r,K x){I i=xn-1;K z=e(r,Xx),y=kK(i--);r=zu,Yx=z;W(i--)Yx=e(0,xK[i+1]);R u
 //!branch out
 ZK b(I f,K x){K y=d(16,x);R Ay?AB("b"):16==yu?yC[yn-1]=JJ[yC[yn-1]+f*4],y:j3(y,tst(t(x),yu),c1(JJ[f?2:6]));}
 
-// force: transform values and other expressions into object code,
-// except conditional expressions, and move result to register r
+//!force: transform values and other expressions into object code, except conditional expressions, and move result to register r
 K f(I r,K x){K y=e(r,x);R r-yu?MV(t(x),r,y):y;}//<! if not a function, move value y to register r
 
 //!ctf
@@ -127,9 +135,10 @@ ZK g(I c,K x){K y=c0(),z,r=c0();I i=0,l=a?M:0;W(++i<xn){z=Xx;I l=M&1<<i,b=Az||12
  if(l)z=j2(b?psh(0,i):z,b?z:psh(0,i)),r=j2(r,pop(0,i));y=j2(z,y);}
  z=cll(c);W(i<16){if(l&1<<i)z=j3(psh(0,i),z,pop(0,i));++i;}R j3(y,z,r);}
 
-//! to register, constant or global
+//!to register, constant or global
 K d(I r,K x){
- P(Ax,(r=q(x))?M|=1<<r,u(r,c0()):x)I s=15&r,a;K y,z;
+ P(Ax,(r=q(x))?M|=1<<r,u(r,c0()):x)
+ I s=15&r,a;K y,z;
  S((y=xy,c(a=*xC)), //!< a is function, y is 1st argument
   case'N':
   C('W',R w(x))
@@ -176,7 +185,23 @@ K d(I r,K x){
  )}
 
 //!opcode length
-ZI ln(S s){I o=*s++,h=o/16,p=0xc5==o?2:0x0f==o;R 4==h?1+ln(s):RET==o||5==h?1:*JJ==o||7==h?2:0xe==h||0xb==h?5:p&&8==*s/16?6:p+(3==s[p]/64?2+(0x83==o||0x6b==o):5==(0xc7&s[p])?6:3);}
+ZI ln(S s){I o=*s++,      //!< opcode
+    h=o/16,               //!< first hex digit of opcode
+    p=0xc5==o?2:0x0f==o;  //!< add 2 for invalid opcode, 1 for 2 byte opcodes
+    R 
+     4==h?1+ln(s):        //!< h=4 rex
+     RET==o||5==h?1:      //!< h=5 push/pop
+     *JJ==o||7==h?2:      //!< h=7 conditional jumps
+     0xe==h||0xb==h?5:    //!< h=0xe call, h=0xb mov
+     p&&8==*s/16?6:       //!< conditional jumps
+     // 3==s[p]/64 => mod == 11 => r/m is a register
+     //               add      imul
+     p+(3==s[p]/64?2+(0x83==o||0x6b==o):
+     // displacement addressing mode: the instruction
+     // will have one instruction byte, one for mod,reg,r/m
+     // and four for the address, else instruction, mod,reg,r/m
+     // and one byte for the register
+     5==(0xc7&s[p])?6:3);}
 
 //!fix relative addresses
 V1(lnk){S s=xC;W(s<xC+xn){
